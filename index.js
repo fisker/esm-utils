3a8e8fa4e1b54d59c505fc021dee64e13f252976
fisker <lionkay@gmail.com>
2021-05-15 13:37:11 +0800

    Export style

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import create from './lib/utils.js'
 import simulatedImportMeta from './lib/simulated-import-meta.js'
 
-export default create
-export const utils = create(simulatedImportMeta)
+const utils = create(simulatedImportMeta)
+
+export {create as default, utils}
