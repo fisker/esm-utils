43fa1143f57ac991a6dc4e00f73d91d55cc24035
fisker Cheung <lionkay@gmail.com>
2021-08-30 12:32:30 +0800

    Remove `utils` export [Reverts #28] (#87)

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import create from './lib/utils.js'
-import simulatedImportMeta from './lib/simulated-import-meta.js'
-
-const utils = create(simulatedImportMeta)
-
-export {create as default, utils}
+export {default} from './lib/utils.js'
