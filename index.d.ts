f5b09dfe8440dee791e77b723a7b248496bfd39d
fisker Cheung <lionkay@gmail.com>
2021-12-28 10:44:25 +0800

    Tweak

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,35 @@
-export interface JSONLoader {
-  load (file: string | URL): Promise<Record<string, any>>
-  loadSync (file: string | URL): Record<string, any>
+// From https://github.com/sindresorhus/type-fest
+type JsonValue = string | number | boolean | null | {[Key in string]?: JsonValue} | JsonValue[]
+
+export interface JsonUtils {
+  /**
+  Read and parse a JSON file.
+
+  @example
+  ```
+  import createEsmUtils from 'esm-utils'
+  const {json} = createEsmUtils(import.meta)
+
+  const data = await json.load('foo.json')
+  ```
+  */
+  load (file: string | URL): Promise<JsonValue>
+  /**
+  Read and parse a JSON file.
+
+  @example
+  ```
+  import createEsmUtils from 'esm-utils'
+  const {json} = createEsmUtils(import.meta)
+
+  const data = json.loadSync('foo.json')
+  ```
+  */
+  loadSync (file: string | URL): JsonValue
 }
 
 /**
-Create a utilities you'll need when migrating to ESModule.
+Create utilities for ES Module.
 
 @param importMeta import.meta
 
@@ -14,14 +39,14 @@ import createEsmUtils from 'esm-utils'
 
 const esmUtils = createEsmUtils(import.meta)
 ```
- */
+*/
 export default function createEsmUtils (
   importMeta: ImportMeta,
 ): {
   readonly filename: string
   readonly dirname: string
   readonly require: NodeRequire
-  readonly json: JSONLoader
+  readonly json: JsonUtils
   readonly __filename: string
   readonly __dirname: string
 }
